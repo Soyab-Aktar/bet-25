b4c8b493dae1f82441d0ad357fbe5b704e1dc910
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-04-22 08:51:22 +0530

    Assets added , add header, add hero, add pageSwitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Header/Navbar";
+import Hero from "./Components/Hero/Hero";
+import SwitchBtn from "./Components/PageSwitch/SwitchBtn";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [money, setMoney] = useState(0);
+  const [activeBtn, setActiveBtn] = useState("Available");
+  const handleAddMoney = () => {
+    const newMoney = money + 1000000;
+    setMoney(newMoney);
+  };
+  const handleBtnSwitch = (tab) => {
+    setActiveBtn(tab);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="sora-font">
+      <div className="mx-auto max-w-screen-xl">
+        <Navbar money={money}></Navbar>
+        <Hero handleAddMoney={handleAddMoney}></Hero>
+        <SwitchBtn
+          handleBtnSwitch={handleBtnSwitch}
+          activeBtn={activeBtn}
+        ></SwitchBtn>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/PageSwitch/SwitchBtn.jsx
@@ -0,0 +1,27 @@
+const SwitchBtn = ({ handleBtnSwitch, activeBtn }) => {
+  return (
+    <div className="flex justify-between">
+      <h1 className="font-bold text-3xl">Available Players</h1>
+      <div>
+        <button
+          onClick={() => handleBtnSwitch("Available")}
+          className={`px-7 py-3.5 rounded-md  rounded-r-none ${
+            activeBtn === "Available" ? "bg-lime-400" : "bg-gray-300"
+          }`}
+        >
+          Available
+        </button>
+        <button
+          onClick={() => handleBtnSwitch("Selected")}
+          className={`px-7 py-3.5 rounded-md  rounded-l-none ${
+            activeBtn === "Selected" ? "bg-lime-400" : "bg-gray-300"
+          }`}
+        >
+          Selected
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SwitchBtn;
